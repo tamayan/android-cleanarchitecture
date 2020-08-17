2292757b6586fffa1e3798f0aea9a06c53ff6ed4
tamayan <tamayan.db18+dev@gmail.com>
2020-08-17 20:06:03 +0900

    DIに実装

--- app/src/main/java/com/example/cleanarchitecture/data/datastore/cloud/CloudNewsDataStore.kt
+++ app/src/main/java/com/example/cleanarchitecture/data/datastore/cloud/CloudNewsDataStore.kt
@@ -1,9 +1,6 @@
 package com.example.cleanarchitecture.data.datastore.cloud
 
 import com.example.cleanarchitecture.data.datastore.NewsDataStore
-import com.example.cleanarchitecture.data.datastore.cloud.client.ApiClient
-import com.example.cleanarchitecture.data.entity.json.NewsRequestEntity
-import com.example.cleanarchitecture.data.entity.json.NewsResponseEntity
 import com.example.cleanarchitecture.domain.entity.News
 import io.reactivex.Observable
 import io.reactivex.Single
@@ -12,11 +9,11 @@ import io.reactivex.Single
  * Created by MSnowRobin016 on 2017/11/25.
  */
 
-class CloudNewsDataStore(private val client: ApiClient<NewsRequestEntity, List<NewsResponseEntity>>) : NewsDataStore {
+class CloudNewsDataStore(private val newsApi: NewsApi) : NewsDataStore {
 
     override fun getNewsList(): Single<List<News>> {
-        return client
-                .request(NewsRequestEntity())
+        return newsApi
+                .getNewsList()
                 .flatMapObservable { Observable.fromIterable(it) }
                 .map { News(id = it.id, title = it.title, text = it.text) }
                 .toList()
--- app/src/main/java/com/example/cleanarchitecture/di/ApplicationModule.kt
+++ app/src/main/java/com/example/cleanarchitecture/di/ApplicationModule.kt
@@ -3,13 +3,17 @@ package com.example.cleanarchitecture.di
 import android.content.Context
 import androidx.room.Room
 import com.example.cleanarchitecture.BuildConfig
-import com.example.cleanarchitecture.data.datastore.cloud.client.okhttp.BasicCredentialProvider
 import com.example.cleanarchitecture.data.datastore.disk.db.AppDatabase
-import com.example.cleanarchitecture.data.repository.BasicCredentialProviderImpl
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
+import okhttp3.Credentials
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 /**
@@ -21,8 +25,21 @@ object ApplicationModule {
 
     @Singleton
     @Provides
-    fun provideBasicCredentialProvider(): BasicCredentialProvider =
-            BasicCredentialProviderImpl()
+    fun provideOkHttp(): OkHttpClient =
+            OkHttpClient
+                    .Builder()
+                    .addNetworkInterceptor { chain ->
+                        val request = chain
+                                .request()
+                                .newBuilder()
+                                .addHeader("Authorization", Credentials.basic(BuildConfig.BASIC_USER_NAME, BuildConfig.BASIC_PASS))
+                                .build()
+                        chain.proceed(request)
+                    }
+                    .connectTimeout(10, TimeUnit.SECONDS)
+                    .writeTimeout(10, TimeUnit.SECONDS)
+                    .readTimeout(10, TimeUnit.SECONDS)
+                    .build()
 
     @Singleton
     @Provides
@@ -30,6 +47,17 @@ object ApplicationModule {
             Moshi
                     .Builder()
                     .add(KotlinJsonAdapterFactory())
+                    .build()
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(httpClient: OkHttpClient, moshi: Moshi): Retrofit =
+            Retrofit
+                    .Builder()
+                    .baseUrl(BuildConfig.BASE_URL)
+                    .client(httpClient)
+                    .addConverterFactory(MoshiConverterFactory.create(moshi))
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                     .build()
 
     @Singleton
