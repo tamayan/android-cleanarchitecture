18c1563181b2fd187c20cfe275d67ae59e60d012
tamayan <tamayan.db18+dev@gmail.com>
2020-08-21 17:58:57 +0900

    修正

--- app/src/main/java/com/example/cleanarchitecture/data/database/NewsDataStore.kt
+++ app/src/main/java/com/example/cleanarchitecture/data/database/NewsDataStore.kt
@@ -5,31 +5,31 @@ import com.example.cleanarchitecture.domain.domain.news.News
 
 class NewsDataStore(private val appDatabase: AppDatabase) : NewsDataStoreInterface {
 
-//    private val newsDao = appDatabase.newsDao()
+    private val newsDao = appDatabase.newsDao()
 
     override suspend fun save(news: News) {
         appDatabase.withTransaction {
-            appDatabase.newsDao().insertOrUpdate(mapToNewsEntity(news))
+            newsDao.insertOrUpdate(mapToNewsEntity(news))
         }
     }
 
     override suspend fun save(newsList: List<News>) {
         appDatabase.withTransaction {
-            appDatabase.newsDao().insertOrUpdate(newsList.map { mapToNewsEntity(it) })
+            newsDao.insertOrUpdate(newsList.map { mapToNewsEntity(it) })
         }
     }
 
     override suspend fun replaceAll(newsList: List<News>) {
         appDatabase.withTransaction {
-            appDatabase.newsDao().deleteAndInsert(newsList.map { mapToNewsEntity(it) })
+            newsDao.deleteAndInsert(newsList.map { mapToNewsEntity(it) })
         }
     }
 
     override suspend fun find(id: Int): News =
-            mapToNews(appDatabase.newsDao().find(id))
+            mapToNews(newsDao.find(id))
 
     override suspend fun findAll(): List<News> =
-            appDatabase.newsDao().findAll().map { mapToNews(it) }
+            newsDao.findAll().map { mapToNews(it) }
 
     private fun mapToNewsEntity(news: News): NewsEntity =
             NewsEntity(news.id, news.title, news.text)
