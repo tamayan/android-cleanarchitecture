d9dd9cc63d7ec75817acb86852b13f75574374b9
tamayan <tamayan.db18+dev@gmail.com>
2020-08-26 17:16:10 +0900

    emitSource化

--- app/src/main/java/com/example/cleanarchitecture/ui/news/list/NewsListViewModel.kt
+++ app/src/main/java/com/example/cleanarchitecture/ui/news/list/NewsListViewModel.kt
@@ -2,20 +2,15 @@ package com.example.cleanarchitecture.ui.news.list
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.liveData
 import com.example.cleanarchitecture.usecase.news.list.GetNewsListRequest
 import com.example.cleanarchitecture.usecase.news.list.GetNewsListUseCase
 import com.example.cleanarchitecture.usecase.news.list.NewsListModel
-import kotlinx.coroutines.flow.collect
 
 class NewsListViewModel(private val getNewsListUseCase: GetNewsListUseCase) : ViewModel() {
 
     val items: LiveData<List<NewsListModel>> = liveData {
-        getNewsListUseCase
-                .handle(GetNewsListRequest())
-                .newsListModels
-                .collect {
-                    emit(it)
-                }
+        emitSource(getNewsListUseCase.handle(GetNewsListRequest()).newsListModels.asLiveData())
     }
 }
