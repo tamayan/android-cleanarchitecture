f4a5a996e9bdc5cb2850bdd85429b32ec20d9b75
tamayan <tamayan.db18+dev@gmail.com>
2020-09-08 20:11:35 +0900

    修正

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,13 +1,5 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-object Dependencies {
-    const val AndroidBuildTools = "com.android.tools.build:gradle:4.0.1"
-}
-
 plugins {
     `kotlin-dsl`
-    `kotlin-dsl-precompiled-script-plugins`
-    kotlin("jvm") version "1.4.0"
 }
 
 repositories {
@@ -16,16 +8,5 @@ repositories {
 }
 
 dependencies {
-    implementation(Dependencies.AndroidBuildTools)
-    implementation(kotlin("stdlib-jdk8"))
-}
-
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
+    implementation("com.android.tools.build:gradle:4.0.1")
 }
